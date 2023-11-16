806d890e62fbe21039d177e791dd794607aa4261
siva.lbf43 <siva.lbf43@gmail.com>
2023-11-16 00:06:18 -0600

    made changes

--- controllers/vehicle.js
+++ controllers/vehicle.js
@@ -63,7 +63,7 @@ exports.vehicle_create_post = async function (req, res) {
       res.send(`{"error": ${err}}`);
    }
 };
-// for a specific Costume.
+// for a specific vehicle.
 exports.vehicle_detail = async function (req, res) {
    console.log("detail" + req.params.id)
    try {
@@ -74,7 +74,7 @@ exports.vehicle_detail = async function (req, res) {
       res.send(`{"error": document for id ${req.params.id} not found`);
    }
 };
-// Handle Costume update form on PUT.
+// Handle vehicle update form on PUT.
 exports.vehicle_update_put = async function (req, res) {
    console.log(`update on id ${req.params.id} with body
 ${JSON.stringify(req.body)}`)
@@ -94,3 +94,86 @@ ${JSON.stringify(req.body)}`)
 failed`);
    }
 };
+// Handle a show one view with id specified by query
+exports.vehicle_view_one_Page = async function(req, res) {
+   console.log("single view for id " + req.query.id)
+   try{
+   result = await vehicle.findById( req.query.id)
+   res.render('vehicledetail', 
+  { title: 'vehicle Detail', toShow: result });
+   }
+   catch(err){
+   res.status(500)
+   res.send(`{'error': '${err}'}`);
+   }
+  };
+
+// Handle vehicle delete on DELETE.
+exports.vehicle_delete = async function(req, res) {
+ console.log("delete " + req.params.id)
+ try {
+ result = await vehicle.findByIdAndDelete( req.params.id)
+ console.log("Removed " + result)
+ res.send(result)
+ } catch (err) {
+ res.status(500)
+ res.send(`{"error": Error deleting ${err}}`);
+ }
+};
+
+// Handle building the view for creating a vehicle.
+// No body, no in path parameter, no query.
+// Does not need to be async
+exports.vehicle_create_Page = function(req, res) {
+ console.log("create view")
+ try{
+ res.render('vehiclecreate', { title: 'vehicle Create'});
+ }
+ catch(err){
+ res.status(500)
+ res.send(`{'error': '${err}'}`);
+ }
+};
+
+// Handle building the view for creating a vehicle.
+// No body, no in path parameter, no query.
+// Does not need to be async
+exports.vehicle_create_Page = function(req, res) {
+   console.log("create view")
+   try{
+   res.render('vehiclecreate', { title: 'vehicle Create'});
+   }
+   catch(err){
+   res.status(500)
+   res.send(`{'error': '${err}'}`);
+   }
+  };
+  
+
+  // Handle building the view for updating a vehicle.
+// query provides the id
+exports.vehicle_update_Page = async function(req, res) {
+   console.log("update view for item "+req.query.id)
+   try{
+   let result = await vehicle.findById(req.query.id)
+   res.render('vehicleupdate', { title: 'Vehicle Update', toShow: result });
+   }
+   catch(err){
+   res.status(500)
+   res.send(`{'error': '${err}'}`);
+   }
+  };
+
+  // Handle a delete one view with id from query
+exports.vehicle_delete_Page = async function(req, res) {
+   console.log("Delete view for id " + req.query.id)
+   try{
+   result = await vehicle.findById(req.query.id)
+   res.render('vehicledelete', { title: 'Vehicle Delete', toShow: 
+  result });
+   }
+   catch(err){
+   res.status(500)
+   res.send(`{'error': '${err}'}`);
+   }
+  };
