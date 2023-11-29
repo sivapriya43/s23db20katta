038ea86da9625ebd6bc52266e24249c76fb667f8
siva.lbf43 <siva.lbf43@gmail.com>
2023-11-29 15:21:28 -0600

    updated changes

--- routes/vehicle.js
+++ routes/vehicle.js
@@ -1,6 +1,16 @@
 var express = require('express');
 const vehicle_controlers= require('../controllers/vehicle');
 var router = express.Router();
+
+// A little function to check if we have an authorized user and continue on 
+//or
+// redirect to login.
+const secured = (req, res, next) => {
+ if (req.user){
+ return next();
+ }
+ res.redirect("/login");
+ }
 /* GET vehicles */
 router.get('/', vehicle_controlers.vehicle_view_all_Page );
 /* GET detail vehicle page */
@@ -12,7 +22,7 @@ router.get('/create', vehicle_controlers.vehicle_create_Page);
 router.get('/create', vehicle_controlers.vehicle_create_Page);
 
 /* GET create update page */
-router.get('/update', vehicle_controlers.vehicle_update_Page);
+router.get('/update', secured, vehicle_controlers.vehicle_update_Page);
 
 /* GET delete vehicle page */
 router.get('/delete', vehicle_controlers.vehicle_delete_Page);
