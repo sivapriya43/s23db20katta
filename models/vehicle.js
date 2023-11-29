d3f8deb5bf473d3e46bf55dccd21da9531e2adf2
siva.lbf43 <siva.lbf43@gmail.com>
2023-11-29 16:03:13 -0600

    added bonus

--- models/vehicle.js
+++ models/vehicle.js
@@ -8,7 +8,16 @@ const vehicleSchema = mongoose.Schema({
     min: [1000, 'The cost must be at least 1000'], // Minimum cost allowed
     max: [100000, 'The cost must not exceed 100000'], // Maximum cost allowed
   },
-  company: String
+  company: String,
+  // Add more attributes below
+  color: String,
+  model: String,
+  year: {
+    type: Number,
+    min: [1900, 'Invalid year'], // Assuming a reasonable minimum year
+    max: [new Date().getFullYear(), 'Invalid year'], // Assuming the current year as the maximum
+  },
+  // Add other attributes as needed
 })
 
 module.exports = mongoose.model("Vehicle", vehicleSchema)
