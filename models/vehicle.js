3776f8342f06e9087bec5e26f0772529a8c455fe
siva.lbf43 <siva.lbf43@gmail.com>
2023-11-29 15:49:59 -0600

    min max update

--- models/vehicle.js
+++ models/vehicle.js
@@ -1,8 +1,14 @@
 const mongoose = require("mongoose")
+
 const vehicleSchema = mongoose.Schema({
-vehicle_type: String,
-cost: Number,
-company: String
+  vehicle_type: String,
+  cost: {
+    type: Number,
+    required: true,
+    min: [1000, 'The cost must be at least 1000'], // Minimum cost allowed
+    max: [100000, 'The cost must not exceed 100000'], // Maximum cost allowed
+  },
+  company: String
 })
-module.exports = mongoose.model("Vehicle", 
-vehicleSchema)
+
+module.exports = mongoose.model("Vehicle", vehicleSchema)
